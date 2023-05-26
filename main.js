68863120c52f12d20589cd19159c85bfeb85f14a
Islam Hafez <aboeshaeslam2@gmail.com>
2023-05-27 01:22:00 +0300

    Add More Features

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const button = document.querySelector("button");
 button.addEventListener("click", () => {
+   window.onbeforeunload = () => {
+      return "Window is being reloaded";
+   }
    window.location.reload();
 });
